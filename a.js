0b307a2c2ce638015e62364f79e95199c1cd8d0e
ratnadeep2003 <ratnadeepabitkar@gmail.com>
2026-09-04 10:29:35 +0530

    understood type inference

--- a.js
+++ a.js
@@ -1,4 +1,25 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const x = 101;
+const x = 1;
 console.log(x);
+function greet(firstname) {
+    console.log(`hello ${firstname}`);
+}
+greet('ratnadeep'); //argument must be a string
+//* here                
+function sum(a, b) {
+    //console.log(`sum is ${a+b}`) returns void cause its not returning anything back to the function
+    return a + b; //to explicitly say that function must return a number, declare at function defination *
+}
+//but even without explicitly mentioning : number at the function defination, ts can identify that its return value must be a number because ts has type inference 
+//Type inference is TypeScript's built-in functionality that automatically figure out (infer) data types for you, so you don't always have to write them out manually.
+sum(2, 3);
+function isLegal(age) {
+    if (age > 18) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+console.log(isLegal(21));
--- a.ts
+++ a.ts
@@ -7,3 +7,24 @@ function greet(firstname: string){//parameters in functions should have a type
 }
 
 greet('ratnadeep'); //argument must be a string
+
+                                    //* here                
+function sum(a: number, b: number): number{
+    //console.log(`sum is ${a+b}`) returns void cause its not returning anything back to the function
+    return a+b; //to explicitly say that function must return a number, declare at function defination *
+}
+
+//but even without explicitly mentioning : number at the function defination, ts can identify that its return value must be a number because ts has type inference 
+//Type inference is TypeScript's built-in functionality that automatically figure out (infer) data types for you, so you don't always have to write them out manually.
+
+sum(2,3);
+
+
+function isLegal(age: number): boolean{
+    if(age>18){
+        return true;
+    }else{
+        return false;
+    }
+}
+console.log(isLegal(21));
